ae9702c332ac4ad296432db15b5f3430022ed717
Tanmay Sharma <159008441+tanmaysharma1337@users.noreply.github.com>
2024-04-23 23:43:45 +0530

    Update RaspberryPiVideo.py

--- Backend/RaspberryPiVideo.py
+++ Backend/RaspberryPiVideo.py
@@ -1,7 +1,3 @@
-# Web streaming example
-# Source code from the official PiCamera package
-# http://picamera.readthedocs.io/en/latest/recipes2.html#web-streaming
-
 import io
 import picamera
 import logging
